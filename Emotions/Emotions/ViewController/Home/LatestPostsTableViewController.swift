184ca6a43c0e2e8b46522eb061606de4ccde1642
Developer-Paul-t <phs880623@gmail.com>
2021-04-23 14:28:28 +0900

    comments update complete

--- Emotions/Emotions/ViewController/Home/LatestPostsTableViewController.swift
+++ Emotions/Emotions/ViewController/Home/LatestPostsTableViewController.swift
@@ -67,8 +67,8 @@ class LatestPostsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: postCell, for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
         let post = DataManager.shared.latestposts[indexPath.row]
-        let comments = CommentManager.downloadComment(post: post)
-        cell.updateUI(post: post, comments: comments)
+//        let comments = CommentManager.downloadComment(post: post)
+        cell.updateUI(post: post)
         
         // 네트워크 호출시에 이곳에서 데이터 변경하도록 호출 그게 완료되면 보여지는게 바뀌도록
         cell.heartButtonCompletion = { currentHeartState in
--- Emotions/Emotions/ViewController/Home/StarTableViewController.swift
+++ Emotions/Emotions/ViewController/Home/StarTableViewController.swift
@@ -34,8 +34,8 @@ class StarTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: postCell, for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
         let post = PostManager.shared.starPosts[indexPath.row]
-        let comments = CommentManager.downloadComment(post: post)
-        cell.updateUI(post: post, comments: comments)
+//        let comments = CommentManager.downloadComment(post: post)
+        cell.updateUI(post: post)
         
         // 네트워크 호출시에 이곳에서 데이터 변경하도록 호출 그게 완료되면 보여지는게 바뀌도록
         cell.heartButtonCompletion = { currentHeartState in
